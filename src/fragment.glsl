d87b102da3f6eeabf79e348e793498bac1b1cd66
Alex Lagerstedt <alex.lagerstedt@gmail.com>
2024-03-07 14:30:19 +0200

    Draw points as circles

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -6,11 +6,23 @@ flat varying vec3 rgbColor;
 
 void main() {
 
+    // classification color
     if (cls == 0u) {
         gl_FragColor = vec4(0.5, 0.25, 0.1, 1.0);
     } else {
         gl_FragColor = vec4(uColor, 1.0);
     }
 
-    gl_FragColor = vec4(rgbColor, 1.0);
+    // round points
+    float u = 2.0 * gl_PointCoord.x - 1.0;
+    float v = 2.0 * gl_PointCoord.y - 1.0;
+    float cc = u*u + v*v;
+    if(cc > 1.0) {
+        discard;
+    }
+
+    // rgb color
+    if (rgbColor.xyz != vec3(0.0, 0.0, 0.0)) {
+        gl_FragColor = vec4(rgbColor, 1.0);
+    }
 }
