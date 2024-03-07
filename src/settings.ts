fabbc80dd432e53537e3f9d232219b094713f63e
Alex Lagerstedt <alex.lagerstedt@gmail.com>
2024-03-07 14:30:19 +0200

    Toggle color mode from keyboard

--- src/settings.ts
+++ src/settings.ts
@@ -2,3 +2,9 @@ export const CAMERA_NEAR = 0.1;
 export const CAMERA_FAR = 10_000.0;
 
 export const PIXEL_RATIO = Math.min(2, window.devicePixelRatio);
+
+export const COLOR_MODE = {
+    INTENSITY: "0",
+    CLASSIFICATION: "1",
+    RGB: "2",
+};
