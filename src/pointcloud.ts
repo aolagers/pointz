1dacbfb732c0753cad1d96eb141a56d0ad9249e4
Alex Lagerstedt <alex.lagerstedt@gmail.com>
2024-03-07 14:30:19 +0200

    Refactor point data loading
    PointCloudNodes and loading now controlled by PointCloud class

--- src/pointcloud.ts
+++ src/pointcloud.ts
@@ -1,16 +1,35 @@
-import { BufferGeometry, Float32BufferAttribute, Uint32BufferAttribute, Uint8BufferAttribute, Vector3 } from "three";
+import {
+    BufferGeometry,
+    Float32BufferAttribute,
+    Points,
+    Uint32BufferAttribute,
+    Uint8BufferAttribute,
+    Vector3,
+} from "three";
 import workerUrl from "./worker?worker&url";
-import type { WorkerInfoRequest, WorkerInfoResponse, WorkerPointsRequest, WorkerPointsResponse, LazSource } from "./worker";
+import type {
+    WorkerInfoRequest,
+    WorkerInfoResponse,
+    WorkerPointsRequest,
+    WorkerPointsResponse,
+    LazSource,
+    CopcNodeInfo,
+    Hierarchy,
+} from "./worker";
+import { MATERIALS } from "./materials";
+import { Viewer } from "./viewer";
 
 export class PointCloudNode {
     nodeName: string;
     geometry: BufferGeometry;
     bounds: { min: Vector3; max: Vector3 };
+    pco: Points;
 
     constructor(name: string, geom: BufferGeometry, bounds: { min: Vector3; max: Vector3 }) {
         this.nodeName = name;
         this.geometry = geom;
         this.bounds = bounds;
+        this.pco = new Points(this.geometry, MATERIALS.POINT);
     }
 }
 
@@ -35,28 +54,83 @@ function getInfo(worker: Worker, source: LazSource) {
     });
 }
 
+function getData(worker: Worker, source: LazSource, node: CopcNodeInfo, offset: number[]) {
+    return new Promise<{ geometry: BufferGeometry }>((resolve, reject) => {
+        worker.onmessage = (e) => {
+            const data = e.data as WorkerPointsResponse;
+            if (data.msgType === "points") {
+                const geometry = new BufferGeometry();
+                geometry.setAttribute("position", new Float32BufferAttribute(data.positions, 3));
+                geometry.setAttribute("color", new Uint8BufferAttribute(data.colors, 3, true));
+                const classes = Array(data.pointCount).fill(2);
+                geometry.setAttribute("classification", new Uint32BufferAttribute(classes, 1));
+                worker.terminate();
+                resolve({
+                    geometry: geometry,
+                });
+            } else {
+                reject("not points");
+            }
+        };
+        const req: WorkerPointsRequest = {
+            command: "load-node",
+            source: source,
+            node: node,
+            offset: offset,
+        };
+        worker.postMessage(req);
+    });
+}
+
 export class PointCloud {
-    // geometry: BufferGeometry;
+    viewer: Viewer;
     name: string;
     source: string | File;
     offset: Vector3;
     bounds: { min: Vector3; max: Vector3 };
+    hierarchy: Hierarchy;
+    loadedNodes: PointCloudNode[];
 
-    nodes: PointCloudNode[];
-    hierarchy: { name: string; pointCount: number }[];
-
-    constructor(name: string, source: string | File, bounds: { min: Vector3; max: Vector3 }, offset: Vector3) {
+    constructor(
+        viewer: Viewer,
+        name: string,
+        source: string | File,
+        bounds: { min: Vector3; max: Vector3 },
+        offset: Vector3,
+        hierarchy: Hierarchy,
+    ) {
+        this.viewer = viewer;
         this.name = name;
         this.source = source;
         this.offset = offset;
         this.bounds = bounds;
-        this.nodes = [];
-        this.hierarchy = [];
+        this.loadedNodes = [];
+        this.hierarchy = hierarchy;
     }
 
-    static async loadLAZ(source: string | File) {
-        const info = await PointCloud.loadInfo(source);
+    async loadFake() {
+        const pcn = this.loadedNodes[0]!;
+        this.viewer.scene.add(pcn.pco);
+        this.viewer.objects.push(pcn.pco);
+    }
 
+    async load() {
+        const worker = new Worker(workerUrl, { type: "module" });
+
+        const N = "0-0-0-0";
+        console.log("HIER", this.hierarchy.nodes, this.hierarchy.nodes[N]);
+        const data = await getData(worker, this.source, this.hierarchy.nodes[N]!, this.offset.toArray());
+
+        const pcn = new PointCloudNode(N, data.geometry, this.bounds);
+
+        this.loadedNodes.push(pcn);
+
+        this.viewer.scene.add(pcn.pco);
+        this.viewer.objects.push(pcn.pco);
+    }
+
+    static async loadLAZ(viewer: Viewer, source: string | File) {
+        const info = await PointCloud.loadInfo(source);
 
         if (info.msgType !== "info") {
             throw new Error("not info");
@@ -68,50 +142,21 @@ export class PointCloud {
             max: new Vector3(...info.header.max),
         };
 
-        const pc = new PointCloud("pc-1", source, bounds, offset);
+        const pc = new PointCloud(viewer, "pc-1", source, bounds, offset, info.hierarchy);
 
-        const worker = new Worker(workerUrl, { type: "module" });
-
-        return new Promise<PointCloud>((resolve, reject) => {
-            worker.onmessage = (e) => {
-                console.log("FROM WORKER", e.data);
-                const data = e.data as WorkerPointsResponse;
-                if (data.msgType === "points") {
-                    const geometry = new BufferGeometry();
-                    geometry.setAttribute("position", new Float32BufferAttribute(data.positions, 3));
-                    geometry.setAttribute("color", new Uint8BufferAttribute(data.colors, 3, true));
-
-                    const classes = Array(data.pointCount).fill(2);
-                    geometry.setAttribute("classification", new Uint32BufferAttribute(classes, 1));
-                    // geometry.setAttribute("classification", new Uint8BufferAttribute(data.classifications, 1));
-                    const rootNode = new PointCloudNode("0-0-0-0", geometry, bounds);
-                    pc.nodes.push(rootNode);
-                    worker.terminate();
-                    resolve(pc);
-                } else {
-                    reject("not points");
-                }
-            };
-
-            const req: WorkerPointsRequest = {
-                command: "load-node",
-                source: source,
-                node: info.hierarchy.nodes["0-0-0-0"]!,
-                offset: info.header.offset,
-            };
-
-            worker.postMessage(req);
-        });
+        return pc;
     }
 
     static async loadInfo(source: LazSource) {
         const worker = new Worker(workerUrl, { type: "module" });
         const info = await getInfo(worker, source);
+        console.log("COPC INFO", info);
+
         worker.terminate();
         return info;
     }
 
-    static loadDemo() {
+    static loadDemo(viewer: Viewer) {
         const geometry = new BufferGeometry();
         const vertices = [];
         const colors = [];
@@ -125,7 +170,7 @@ export class PointCloud {
             return 2 * (Math.random() - 0.5);
         }
         // gnd
-        for (let i = 0; i < 10_000; i++) {
+        for (let i = 0; i < 50_000; i++) {
             const x = r() * 50;
             const y = r() * 50;
             const z = 2 * Math.sin(x / 10) + 1 * Math.sin(y / 5);
@@ -135,18 +180,15 @@ export class PointCloud {
         }
 
         // trees
-        const treeH = 12;
-        const treePts = 100;
+        const treePts = 256;
         for (let i = 0; i < 24; i++) {
+            const treeH = 8 + 10 * Math.random();
             const x = (Math.random() - 0.5) * 100;
             const y = (Math.random() - 0.5) * 100;
             const z = 2 * Math.sin(x / 10) + 1 * Math.sin(y / 5);
             for (let j = 0; j < treePts; j++) {
-                vertices.push(
-                    x + (r() * (treePts - j)) / treePts,
-                    y + (r() * (treePts - j)) / treePts,
-                    z + (j / treePts) * treeH,
-                );
+                let h = (j / treePts) * treeH;
+                vertices.push(x + (r() * (treePts - j)) / treePts, y + (r() * (treePts - j)) / treePts, z + h);
                 colors.push(0.1 + Math.random() * C, 0.7 + Math.random() * C, 0.1 + Math.random() * C);
                 classes.push(1);
             }
@@ -168,10 +210,10 @@ export class PointCloud {
         geometry.setAttribute("classification", new Uint32BufferAttribute(classes, 1));
 
         const bounds = { min, max };
-        const node = new PointCloudNode("0-0-0-0", geometry, bounds);
 
-        const pc = new PointCloud("demodata", "", bounds, offset);
-        pc.nodes.push(node);
+        const pc = new PointCloud(viewer, "demodata", "", bounds, offset, { pages: {}, nodes: {} });
+
+        pc.loadedNodes.push(new PointCloudNode("0-0-0-0", geometry, bounds));
 
         return pc;
     }
