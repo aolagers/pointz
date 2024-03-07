bb5cdf5b6dbecd4b550ff7fc478201ad2dc5180a
Alex Lagerstedt <alex.lagerstedt@comtiki.fi>
2024-03-07 14:30:19 +0200

    Prioritize loading by distance

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,11 @@ const viewer = new Viewer();
 
 viewer.init();
 
-viewer.start();
+viewer.renderLoop();
+
+viewer.addDemo();
+// viewer.addLAZ("http://localhost:5173/copc.copc.laz");
+viewer.addLAZ("http://localhost:5173/lion_takanawa.copc.laz");
+viewer.addLAZ("http://localhost:5173/autzen-classified.copc.laz");
+// viewer.addLAZ("http://localhost:5173/sofi.copc.laz");
+// viewer.addLAZ("http://localhost:5173/millsite.copc.laz");
