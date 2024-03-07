24f1cc3cec6de48f707938f0dac39a42cba65316
Alex Lagerstedt <alex.lagerstedt@gmail.com>
2024-03-07 14:30:19 +0200

    Use shader, add some code structure

--- src/main.ts
+++ src/main.ts
@@ -11,16 +11,98 @@ import {
     ShaderMaterial,
     Color,
     PointsMaterial,
+    Camera,
 } from "three";
 
-const scene = new Scene();
+class PointCloud {
+    static load() {
+        const geometry = new BufferGeometry();
+        const vertices = [];
 
-const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        for (let i = 0; i < 1_000; i++) {
+            const x = (Math.random() - 0.5) * 5;
+            const y = (Math.random() - 0.5) * 5;
+            const z = (Math.random() - 0.5) * 5;
+
+            vertices.push(x, y, z);
+        }
+
+        geometry.setAttribute("position", new Float32BufferAttribute(vertices, 3));
+
+        return geometry;
+    }
+
+    static getMaterial() {
+        const vertexShader = `
+            void main() {
+                vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
+
+                gl_PointSize = 2.0;
+                gl_Position = projectionMatrix * mvPosition;
+            }
+        `;
+
+        const fragmentShader = `
+            void main() {
+                gl_FragColor = vec4( 1.0, 0.0, 0.0, 1.0 );
+            }
+        `;
+
+        const smaterial = new ShaderMaterial({
+            uniforms: {
+                color: { value: new Color(0xff0000) },
+                position: {},
+            },
+            vertexShader: vertexShader,
+            fragmentShader: fragmentShader,
+        });
+
+        const pmaterial = new PointsMaterial({
+            size: 5,
+        });
+
+        return smaterial;
+    }
+}
+
+class World {
+    scene: Scene;
+    cube: Mesh;
+
+    constructor() {
+        this.scene = new Scene();
+
+        this.cube = getCube();
+        this.scene.add(this.cube);
+
+        const pcloud = new Points(PointCloud.load(), PointCloud.getMaterial());
+        this.scene.add(pcloud);
+    }
+
+    update(camera: Camera) {
+        camera.position.z = 5;
+        //cube.rotation.x += 0.01;
+        //cube.rotation.y += 0.01;
+        camera.position.x = Math.sin(Date.now() / 2_000);
+        camera.position.y = Math.sin(Date.now() / 5_000);
+        camera.lookAt(this.cube.position);
+    }
+}
+
+function getCube() {
+    const geometry = new BoxGeometry(1.0, 0.1, 0.1);
+    const material = new MeshBasicMaterial({ color: 0x00ff00 });
+
+    const cube = new Mesh(geometry, material);
+    return cube;
+}
 
 const renderer = new WebGLRenderer();
 
+const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setPixelRatio(window.devicePixelRatio);
 
 document.body.appendChild(renderer.domElement);
 
@@ -29,99 +111,20 @@ document.body.addEventListener("pointermove", onPointerMove);
 
 window.addEventListener("resize", onWindowResize);
 
-const geometry = new BoxGeometry(1, 1, 1);
-const material = new MeshBasicMaterial({ color: 0x00ff00 });
-
-const cube = new Mesh(geometry, material);
-
-const VERT = `
-attribute float size;
-attribute vec3 customColor;
-
-varying vec3 vColor;
-
-void main() {
-
-    vColor = customColor;
-
-    vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
-
-    gl_PointSize = size * ( 300.0 / -mvPosition.z );
-
-    gl_Position = projectionMatrix * mvPosition;
-
-}`;
-
-const FRAG = `
-uniform vec3 color;
-uniform sampler2D pointTexture;
-uniform float alphaTest;
-
-varying vec3 vColor;
-
-void main() {
-
-    gl_FragColor = vec4( color * vColor, 1.0 );
-
-    gl_FragColor = gl_FragColor * texture2D( pointTexture, gl_PointCoord );
-
-    if ( gl_FragColor.a < alphaTest ) discard;
-
-}`;
-
-scene.add(cube);
-
-camera.position.z = 5;
-
 let mouseX = 0;
 let mouseY = 0;
 
 let windowHalfX = window.innerWidth / 2;
 let windowHalfY = window.innerHeight / 2;
 
-const bgeometry = new BufferGeometry();
-const vertices = [];
+const world = new World();
 
-for (let i = 0; i < 1_000; i++) {
-    const x = (Math.random() - 0.5) * 1000;
-    const y = (Math.random() - 0.5) * 1000;
-    const z = (Math.random() - 0.5) * 1000;
+function loop() {
+    requestAnimationFrame(loop);
 
-    vertices.push(x, y, z);
-}
+    world.update(camera);
 
-bgeometry.setAttribute("position", new Float32BufferAttribute(vertices, 3));
-
-// const bmaterial = new ShaderMaterial({
-//     uniforms: {
-//         color: { value: new Color(0xff0000) },
-//     },
-//     vertexShader: VERT,
-//     fragmentShader: FRAG,
-// });
-//
-const bmaterial = new PointsMaterial({
-    size: 5,
-    // sizeAttenuation: true,
-
-})
-
-const particles = new Points(bgeometry, bmaterial);
-scene.add(particles);
-
-
-function animate() {
-    requestAnimationFrame(animate);
-
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
-    camera.position.x = Math.sin(Date.now()/2_000);
-    camera.position.y = Math.sin(Date.now()/5_000);
-
-    camera.lookAt(cube.position);
-
-    renderer.render(scene, camera);
+    renderer.render(world.scene, camera);
 }
 
 function onWindowResize() {
@@ -132,15 +135,13 @@ function onWindowResize() {
     camera.updateProjectionMatrix();
 
     renderer.setSize(window.innerWidth, window.innerHeight);
-
-    // console.log("resize", window.innerWidth, window.innerHeight, camera.aspect);
 }
 
-function onPointerMove(event) {
+function onPointerMove(event: PointerEvent) {
     if (event.isPrimary === false) return;
 
     mouseX = event.clientX - windowHalfX;
     mouseY = event.clientY - windowHalfY;
-    // console.log("mouse", mouseX, mouseY);
 }
-animate();
+
+loop();
