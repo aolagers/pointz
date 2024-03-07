afae6220d5b32a59f760a97fb502062f501241bb
Alex Lagerstedt <alex.lagerstedt@gmail.com>
2024-03-07 14:30:19 +0200

    Add vitest for unit testing

--- src/priority-queue.ts
+++ src/priority-queue.ts
@@ -78,3 +78,33 @@ export class PriorityQueue<T> {
         return item;
     }
 }
+
+if (import.meta.vitest) {
+    const { it, expect } = import.meta.vitest;
+
+    it("works with custom comparator", () => {
+        const pq = new PriorityQueue<string>((a, b) => a.length - b.length);
+        pq.push("12345");
+        pq.push("x");
+        pq.push("abc");
+        expect(pq.pop()).toBe("x");
+        expect(pq.pop()).toBe("abc");
+        expect(pq.pop()).toBe("12345");
+        expect(pq.pop()).toBe(null);
+    });
+
+    it("order equals sorting an array", () => {
+        const arr = Array(100)
+            .fill(0)
+            .map((_) => Math.random());
+
+        const pq = new PriorityQueue<number>();
+        arr.forEach((item) => pq.push(item));
+
+        arr.sort((a, b) => a - b);
+        for (const v of arr) {
+            expect(v).toEqual(pq.pop());
+        }
+        expect(pq.pop()).toBe(null);
+    });
+}
