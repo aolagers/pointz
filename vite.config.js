0803abf22f912b9e87d43507dd1a5ee9c4295480
Alex Lagerstedt <alex.lagerstedt@gmail.com>
2024-03-07 14:30:19 +0200

    Load real point data from COPC LAZ

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import glsl from "vite-plugin-glsl";
 import { defineConfig } from "vite";
 
+const wasmContentTypePlugin = {
+  name: "wasm-content-type-plugin",
+  configureServer(server) {
+    server.middlewares.use((req, res, next) => {
+      console.log("REQUEST!!!", Date.now(), req.url);
+      if (req.url.endsWith(".wasm")) {
+        res.setHeader("Content-Type", "application/wasm");
+      }
+      next();
+    });
+  },
+};
+
 export default defineConfig({
-  plugins: [glsl()],
+  plugins: [glsl(), wasmContentTypePlugin],
 });
