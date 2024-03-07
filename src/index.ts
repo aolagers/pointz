bfae15da59dae8a321d0a2391e1505706bceaf7d
Alex Lagerstedt <alex.lagerstedt@gmail.com>
2024-03-07 14:30:19 +0200

    Control canvas size from outsize Viewer

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,9 @@ window.onerror = (message, source, lineno, colno, error) => {
     document.body.innerHTML = `<pre>${message}\n${source} ${lineno}:${colno}\n${error}</pre>`;
 };
 
-const viewer = new Viewer();
+const canvas = document.getElementById("viewer") as HTMLCanvasElement;
+
+const viewer = new Viewer(canvas, window.innerWidth, window.innerHeight);
 
 viewer.init();
 
@@ -19,6 +21,9 @@ viewer.addLAZ(here + "/lion_takanawa.copc.laz");
 viewer.addLAZ("https://s3.amazonaws.com/hobu-lidar/autzen-classified.copc.laz");
 
 // viewer.addLAZ("http://localhost:5173/autzen-classified.copc.laz");
+window.addEventListener("resize", () => {
+    viewer.setSize(window.innerWidth, window.innerHeight);
+});
 
 // viewer.addLAZ("https://s3.amazonaws.com/hobu-lidar/sofi.copc.laz");
 // viewer.addLAZ("https://s3.amazonaws.com/data.entwine.io/millsite.copc.laz");
