281562e5160684315ecdd855daf353786c463959
Alex Lagerstedt <alex.lagerstedt@gmail.com>
2024-07-03 23:32:49 +0300

    Fix manifest and worker urls for hosting from subdir

--- viewer/src/index.ts
+++ viewer/src/index.ts
@@ -25,7 +25,7 @@ function stringifyError(e: unknown) {
 
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker
-        .register("/service-worker.js")
+        .register("./service-worker.js")
         .then((registration) => {
             console.log("Service Worker registered with scope:", registration.scope);
 
